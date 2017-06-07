154c27dc39ab5f3b6df89e6c4443719cfe109221
idrissMSF <idriss.ni@gmail.com>
2017-06-07 12:33:34 +0100

    Create template.R
    How code should look like

---
+++ template.R
@@ -0,0 +1,16 @@
+# Title
+# Sources
+# Explanation
+
+
+# install.packages(needed)
+    # necesseray to donwload before starting an offline analysis
+
+# library(pakages needed)
+
+
+# define customized variables, e.g. col.names
+# functions to set correct variable
+
+# actual code finally (commented)
+
